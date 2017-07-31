62dc822dd28d3bd666314f6d3d3cc08a606ba49c
양창엽 <ChangYeop-Yang@users.noreply.github.com>
2017-07-31 15:40:17 +0900

    Initial Commit

---
+++ LazyHue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LazyHue
+//
+//  Created by 양창엽 on 2017. 7. 31..
+//  Copyright © 2017년 Yang-Chang-Yeop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
