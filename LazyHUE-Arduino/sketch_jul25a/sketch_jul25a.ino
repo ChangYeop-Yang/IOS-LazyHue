c0a2a2275c52f9be6a695f7bc04f78735a77adbe
양창엽 <yeop9657@outlook.com>
2018-07-27 19:54:36 +0900

    [#4] Update 2018.07.27. 19:54:09 Update ESP-8266 설정 구현, 소스 코드 리펙토링, 타이머 기능 구현

--- LazyHUE-Arduino/sketch_jul25a/sketch_jul25a.ino
+++ LazyHUE-Arduino/sketch_jul25a/sketch_jul25a.ino
@@ -1,5 +1,7 @@
 #include <dht.h>
+#include <Timer.h>
 #include "MQ135.h"
+#include <SoftwareSerial.h>
 
 // MARK: - DPIN Port Number
 #define TILT_DPIN 11
@@ -18,7 +20,25 @@
 dht DHT;
 
 // MARK: - MQ135
-MQ135 MQ = MQ135(GAS_APIN);
+const MQ135 MQ = MQ135(GAS_APIN);
+
+// MARK: - Timer
+const Timer sensorTimer;
+
+// MARK: - ESP8266 Variable
+const SoftwareSerial esp8266_Serial = SoftwareSerial(2, 3);
+
+// MARK: - Struct
+typedef struct sensory {
+  float humidity = 0.0;
+  float temperature = 0.0;
+
+  int cds = 0;
+  int noise = 0;
+  int co2 = 0;
+} Valsensory;
+
+Valsensory sensoryDatas;
 
 void setup() {
   // put your setup code here, to run once:
@@ -27,6 +47,10 @@ void setup() {
 
   // MARK: DHT11
   DHT.read11(DHT11_DPIN);
+
+  // MARK: ESP8266
+  esp8266_Serial.begin(9600);
+  settingESP8266();
 
   // MARK: 3 Colors LED
   pinMode(LED_BLUE_DPIN, OUTPUT);
@@ -41,29 +65,75 @@ void setup() {
 
   // MARK: Tilt Sensor
   pinMode(TILT_DPIN, INPUT);
+
+  // MARK: setting Collect Sensor Date Timmer 300000
+  sensorTimer.every(60000, collectSensorDate);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  Serial.print("- Humidity (%): ");
-  Serial.println((float)DHT.humidity, 2);
+  // MARK: Detect Tilt Sensor
+  detectTilt();
 
-  Serial.print("- Temperature (°C): ");
-  Serial.println((float)DHT.temperature, 2);
+  // MARK: Update Sensor Timer.
+  sensorTimer.update();
 
-  Serial.print("- CDS (%): ");
-  Serial.println(analogRead(CDS_APIN));
-
-  Serial.print("- SOUND (dB): ");
-  Serial.println(analogRead(SOUND_APIN));
-
-  measureIndoorCO2();
-  
-  delay(1000);
+  // MARK: Push Button
+  sendNetworkButton(digitalRead(BUTTON_DPIN));
 }
 
 // MARK: - Sensory Method
+void settingESP8266() {
+
+    // Setting ESP8266 Configuration
+    esp8266_Serial.println("AT+RST\r");
+    
+    if ( esp8266_Serial.find("OK") ) {
+      Serial.println("⌘ ESP8266 module is operating success.");
+      
+      esp8266_Serial.println("AT+CIOBAUD?\r\n");
+      esp8266_Serial.println("AT+CWMODE=3\r\n");
+      esp8266_Serial.println("AT+CWJAP=\"YCY-Android-Note7\",\"1q2w3e4r!\"\r\n");
+      esp8266_Serial.println("AT+CIPMUX=0\r\n");
+      esp8266_Serial.println("AT+CIPSERVER=1,80\r\n");
+
+      digitalWrite(LED_GREEN_DPIN, HIGH); delay(1000);
+      digitalWrite(LED_GREEN_DPIN, LOW);
+    }
+}
+
+void collectSensorDate() {
+
+  sensoryDatas.humidity = (float)DHT.humidity;
+  sensoryDatas.temperature = (float)DHT.temperature;
+  sensoryDatas.cds = analogRead(CDS_APIN);
+  
+  Serial.print("- Humidity (%): ");
+  Serial.println(sensoryDatas.humidity, 2);
+
+  Serial.print("- Temperature (°C): ");
+  Serial.println(sensoryDatas.temperature, 2);
+
+  Serial.print("- CDS (%): ");
+  Serial.println(sensoryDatas.cds);
+
+  int measurementCO2 = measureIndoorCO2();
+  int measurementNoise = measureIndoorNoise();
+
+  sensoryDatas.co2 = measurementCO2;
+  sensoryDatas.noise = measurementNoise;
+}
+
+void sendNetworkButton(const bool isPush) {
+
+  if (isPush == HIGH) {
+    Serial.println("⌘ 현재 센서 정보를 서버로 전송합니다. 잠시만 기다려주세요.");
+    delay(3000);    
+  }
+  
+}
+
 int measureIndoorCO2() {
 
   const int measurementCO2 = analogRead(MQ.getPPM());
@@ -72,10 +142,68 @@ int measureIndoorCO2() {
   Serial.println(measurementCO2, DEC);
 
   if (measurementCO2 >= 1000) {
-    digitalWrite(LED_RED_DPIN, HIGH);
+    digitalWrite(LED_RED_DPIN, HIGH); delay(3000);
+    digitalWrite(LED_RED_DPIN, LOW);
+    
     Serial.println("⌘ 현재 기준치 초과 실내 대기질 농도가 너무 높습니다.");
   }
   
   return measurementCO2;
 }
 
+int measureIndoorNoise() {
+
+  const int meaurementSound = analogRead(SOUND_APIN);
+
+  Serial.print("- SOUND (dB): ");
+  Serial.println(analogRead(SOUND_APIN));
+
+  if (meaurementSound >= 50) {
+    digitalWrite(LED_BLUE_DPIN, HIGH); delay(3000);
+    digitalWrite(LED_BLUE_DPIN, LOW);
+    
+    Serial.println("⌘ 현재 실내의 소음 기준이 초과하였습니다.");
+  }
+
+  return meaurementSound;
+}
+
+void detectTilt() {
+
+  int tiltValue = digitalRead(TILT_DPIN);
+
+  if (tiltValue == HIGH) {
+    digitalWrite(LED_RED_DPIN, HIGH); delay(3000);
+    digitalWrite(LED_RED_DPIN, LOW);
+
+    Serial.println("⌘ 현재 Arduino에 대한 움직임이 감지 되었습니다.");
+  }
+}
+
+bool sendSensorData(int temp, int cmd, int noise) {
+  
+//  const String basic_serverURL = "coldy24.iptime.org";
+//
+//  if ( esp8266_Serial.find("OK") ) {
+//        Serial.println("- Main Server TCP Connection Ready.");
+//        
+//        String query;
+//        query.concat("GET /setKitchenData?TEMP=");  query.concat(temp);
+//        query.concat("&CMD=");                  query.concat(cmd);  
+//        query.concat("&NOISE=");                query.concat(noise);
+//        query.concat("&FLARE=");                query.concat(currentState.flare_Flag);
+//        query.concat("&GAS=");                  query.concat(analogRead(GAS_APIN));     query.concat("\r\n");
+//    
+//        if ( esp8266_Serial.find(">") ) {
+//            delay(5000);
+//            Serial.println("- Please, Input GET Request Query.");
+//            esp8266_Serial.println(query);
+//          
+//            if ( esp8266_Serial.find("SEND OK") ) {
+//                Serial.println("- Success send server packet.");
+//              }
+//              
+//            esp8266_Serial.println("AT+CIPCLOSE\r");
+//        }
+//  } 
+}
