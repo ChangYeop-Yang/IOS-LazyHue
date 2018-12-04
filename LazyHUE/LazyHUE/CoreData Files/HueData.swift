d0a0bfead350f58aecea867077434849877a8592
ChangYeop-Yang <yeop9657@naver.com>
2018-12-04 22:11:34 +0900

    [#11] Update 2018.12.04. 22:11:03
    [#11] Update 2018.12.04. 22:11:03
    Core Data Draft

---
+++ LazyHUE/LazyHUE/CoreData Files/HueData.swift
@@ -0,0 +1,13 @@
+//
+//  HueData.swift
+//  LazyHUE
+//
+//  Created by 양창엽 on 04/12/2018.
+//  Copyright © 2018 양창엽. All rights reserved.
+//
+
+import CoreData
+
+class HueData: NSObject {
+    
+}
